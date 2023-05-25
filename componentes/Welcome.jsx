4550e31b4440be3450307dc477a3cf0def03af2b
fredfoda <fredhenriquediniz@gmail.com>
2023-05-24 23:35:21 -0300

    atualizado

---
+++ componentes/Welcome.jsx
@@ -0,0 +1,14 @@
+import New from '../assets/oip.jpeg';
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+      <h2>Seja bem vindo</h2>
+      <p>Clique aqui</p>
+      <button>Iniciar</button>
+      <img src="New" alt="Inicio do Quiz" />
+    </div>
+  )
+}
+
+export default Welcome
