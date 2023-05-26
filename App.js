413e9c9aa7322463eb6448d60aff666fbb116af2
fredfoda <fredhenriquediniz@gmail.com>
2023-05-26 19:23:20 -0300

    atualizado

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-// React, componentes, estaticos
-
-
-import "./App.css";
-import Welcome from "./componentes/Welcome";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Quiz de Programação</h1>
-      <Welcome />
-    </div>
+import Quiz from './src/pages/Quiz'
+function App(){
+  return(
+    <Quiz />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
 export default App;
--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -35,3 +35,4 @@ export default function New() {
         </>
     </div>
     );
+  }
